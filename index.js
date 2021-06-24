c3367e4099c35237a5f70e630d3faf2b093965b9
Logan Darby <logandarby4@gmail.com>
2021-06-24 16:29:49 -0400

    Added some comments

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ import typeDefs from "./typeDefs.js";
 import resolvers from "./resolvers.js";
 import { HOST_PORT } from "./constants.js";
 
+// The HOST_PORT is defined in constants.js
 const PORT = process.env.PORT ?? HOST_PORT;
 
+// Setting up the Apollo Server to take GraphQL queries
 const server = new ApolloServer({
   typeDefs,
   resolvers,
