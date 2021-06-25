15dabfbb6172a79d6caa4827c9859c10b5b484cc
Logan <logandarby4@gmail.com>
2021-06-25 10:57:08 -0400

    Basic front-end setup
    - Initialized a create-react-app in ./frontend/
    - Removed unnecessary template code

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello!</h1>
+      <p>Basic front-end setup.</p>
+    </div>
+  );
+}
+
+export default App;
