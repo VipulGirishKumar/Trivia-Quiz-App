46d9444d4832c85f85d51b13671df0bb634ac3f3
Logan <logandarby4@gmail.com>
2021-06-25 15:41:04 -0400

    Added an Express server wrapper
    - Wrapped the existing Apollo GraphQL Server with an Express.js server which hosts the backend on localhost:4000/api
    - Updated the dependencies and removed apollo-server

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,17 +1,30 @@
-import { ApolloServer } from "apollo-server";
+import { ApolloServer } from "apollo-server-express";
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
 import typeDefs from "./typeDefs.js";
 import resolvers from "./resolvers.js";
 import { HOST_PORT } from "./constants.js";
 
-// The HOST_PORT is defined in constants.js
-const PORT = process.env.PORT ?? HOST_PORT;
+// Constants definition
+const PORT = process.env.PORT ?? HOST_PORT; // Defined in constants.js
+const API_PATH = "/api";
+const __dirname = path.dirname(fileURLToPath(import.meta.url)); // Current directory
 
-// Setting up the Apollo Server to take GraphQL queries
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
+// Starting the Express and Apollo Server
+// The Apollo server manages GraphQL requests, and the Express server hosts it
+//  at localhost:4000/api
+const app = express();
+const server = new ApolloServer({ typeDefs, resolvers });
+await server.start();
+server.applyMiddleware({ app, path: API_PATH });
+
+// Redirects any traffic to '/' to the front-end app.
+app.use(express.static(path.join(__dirname, "/../../frontend/build")));
+app.get("*", (_, res) => {
+  res.sendFile(path.join(__dirname + "/../../frontend/build/index.html"));
 });
 
-server.listen({ port: PORT }).then(({ url }) => {
-  console.log(`🚀 Server running at ${url}`);
-});
+// Starting the server
+await new Promise((resolve) => app.listen({ port: PORT }, resolve));
+console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`);
