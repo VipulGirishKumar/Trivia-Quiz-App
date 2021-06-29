e1b4f69ef8c7110c726e52db209f1d32e6af5733
Logan <logandarby4@gmail.com>
2021-06-29 12:05:30 -0400

    Implemented GetQuestions into GraphQL Schema

--- backend/src/typeDefs.js
+++ backend/src/typeDefs.js
@@ -6,8 +6,15 @@ const typeDefs = gql`
     id: ID!
   }
 
+  type Question {
+    question: String!
+    correct_answer: String!
+    incorrect_answers: [String]
+  }
+
   type Query {
     categories: [Category]
+    questions(amount: Int!; categoryID: ID!, difficulty: String!): [Question]
   }
 `;
 
